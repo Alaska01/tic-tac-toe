321188787f29e22ce388668565c4cc5f70858849
Daniduran-dev <danigduran@gmail.com>
2020-08-13 16:52:32 +0700

    delete ;end at line 62

--- bin/main.rb
+++ bin/main.rb
@@ -59,7 +59,7 @@ class GameDynamics
 
   # Check if the move is valid or not and inform the player if its not allowed.
   def valid_move
-    unless valid ? 'Hey this is not allowed!!' : 'Valid move'; end
+    unless valid ? 'Hey this is not allowed!!' : 'Valid move'
   end
   # Move of player one displayed on the board
   puts "Positions available: #{current_board_positions}"
