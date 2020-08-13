5e0469c2095de5002cb83c3e8a56188370e70f64
Daniduran-dev <danigduran@gmail.com>
2020-08-13 16:42:06 +0700

    organize the interface structure

--- bin/main.rb
+++ bin/main.rb
@@ -1,9 +1,7 @@
 #!/usr/bin/env ruby
-
-class TicTacToePseudocode
   # Welcome users and show the board
 
-  current_board_positions = []
+  @@current_board_positions = []
 
   puts 'Welcome to the tic-tac-toe game'
 
@@ -30,11 +28,27 @@ class TicTacToePseudocode
   # Display the current positions that are taken, at the begining it will be empty.
 
   # Method to Select which Player's turn
-
-  if player1 is even
-    puts "#{player1} its your turn"
-  else
-    puts "#{player2} its your turn"
+  class GameDynamics
+    def turn
+      @round = 0
+      while round < 9 do
+        if round.even?
+          # show free positions
+          puts "Player1 choose a free cell"
+          # check if answer is valid
+          # check if the cell is free
+          # return method that will display the board with the chosen position filled
+          # check if it matches a winning combination
+        else
+          # show free positions
+          puts "Player2 choose a free cell"
+          # check if answer is valid
+          # check if the cell is free
+          # return method that will display the board with the chosen position filled
+          # check if it matches a winning combination
+        end
+        round += 1
+      end
   end
 
   # Get inputs by player 1 and player 2
@@ -44,9 +58,9 @@ class TicTacToePseudocode
   # player1_input = gets.chomp
 
   # Check if the move is valid or not and inform the player if its not allowed.
-
-  unless valid ? 'Hey this is not allowed!!' : 'Valid move'
-
+  def valid_move
+    unless valid ? 'Hey this is not allowed!!' : 'Valid move'
+  end
     # Move of player one displayed on the board
     puts "Positions available: #{current_board_positions}"
     puts 'Player Two its your turn'
@@ -57,17 +71,15 @@ class TicTacToePseudocode
     # Check which player is to play next
 
     # Check Winner, if game is not a draw and spaces are all occupied
-
-    while !winner && turn_counter < 9
-      # repeat player selection loop
+    def winner
+      while !winner && turn_counter < 9
+        # repeat player selection loop
+        if winner == player1
+          puts "Congratulations #{player1} won"
+        elsif winner == player2
+          puts "Congratulations #{player2} won"
+        else
+          puts "It's a draw!!"
+        end
+      end
     end
-
-    if winner == player1
-      puts "Congratulations #{player1} won"
-    elsif winner == player2
-      puts "Congratulations #{player2} won"
-    else
-      puts "It's a draw!!"
-    end
-  end
-end
