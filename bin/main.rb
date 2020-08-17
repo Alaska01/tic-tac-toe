a83802340d1642bee2ef14000865d5f85d31d7af
Daniduran-dev <danigduran@gmail.com>
2020-08-17 13:54:46 +0700

    back to the previous model

--- bin/main.rb
+++ bin/main.rb
@@ -1,35 +1,87 @@
-puts 'Welcome to Tic-Tac-Toe!'
-loop do
-  puts 'Are you ready to start a new game?(y/n)'
-  start_game = gets.chomp
-  if start_game == 'y'
-    # Create a new game. Game class method.
-    puts 'Player 1 insert your name'
-    # player1 = gets.chomp
-    puts 'Player 2 insert your name'
-    # player2 = gets.chomp
-    # Game class method to create players.
-    # Game class method to display the board.
-    loop do
-      round = 0
-      # Game class method to check which player has to play.
-      loop do
-        # Game class method to display the board with the available positions.
-        puts 'Please choose a row number and a column letter'
-        # move = gets.chomp
-        # Game class method to validate the move
-        break # move is validated
-      end
-      # Game class method to check if there's a winner
-      puts 'Congratulations player won :)' if winner
-      round += 1 # Can be a method to count rounds
-      next if round == 9
+#!/usr/bin/env ruby
+# Welcome users and show the board
 
-      puts 'Draw :('
-      break
+# current_board_positions = []
+
+puts 'Welcome to the tic-tac-toe game'
+
+puts 'Player One Insert Your name'
+
+# player1 = gets.chomp
+
+puts 'Player Two Enter your name'
+# player2 = gets.chomp
+
+# Below is where the board will be displayed
+
+puts '| || || |'
+puts '-----------'
+puts '| || || |'
+puts '-----------'
+puts '| || || |'
+puts 'The winner occupies any horizontal, vertical or diagonal positions concurrently'
+
+# Tell players the rules of the game and what is expected of them
+
+puts 'Letters, spaces and special characters are not allowed!! Choose a number from 1 to 9'
+
+# Display the current positions that are taken, at the begining it will be empty.
+
+# Method to Select which Player's turn
+class GameDynamics
+  def turn
+    @round = 0
+    while round < 9
+      if round.even?
+        # show free positions
+        puts "Player1 choose a free cell"
+        # check if answer is valid
+        # check if the cell is free
+        # return method that will display the board with the chosen position filled
+        # check if it matches a winning combination
+      else
+        # show free positions
+        puts "Player2 choose a free cell"
+        # check if answer is valid
+        # check if the cell is free
+        # return method that will display the board with the chosen position filled
+        # check if it matches a winning combination
+      end
+      round += 1
     end
-  else
-    puts "The game is over"
-    break
+  end
+
+  # Get inputs by player 1 and player 2
+
+  puts 'Here are the free positions:' # Following we will display the positions that can be taken
+
+  # player1_input = gets.chomp
+
+  # Check if the move is valid or not and inform the player if its not allowed.
+  def valid_move
+    unless valid ? 'Hey this is not allowed!!' : 'Valid move'
+    end
+  end
+  # Move of player one displayed on the board
+  puts "Positions available: #{current_board_positions}"
+  puts 'Player Two its your turn'
+
+  # player2_input = gets.chomp
+  # Display the board progress
+  puts "Positions available: #{current_board_positions}"
+  # Check which player is to play next
+
+  # Check Winner, if game is not a draw and spaces are all occupied
+  def winner
+    while !winner && turn_counter < 9
+      # repeat player selection loop
+      if winner == player1
+        puts "Congratulations #{player1} won"
+      elsif winner == player2
+        puts "Congratulations #{player2} won"
+      else
+        puts "It's a draw!!"
+      end
+    end
   end
 end
