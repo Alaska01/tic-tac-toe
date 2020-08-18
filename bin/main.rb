b3c29db938a899c0cf375009ac9114868923e2af
Alaska01 <aadaniel108@gmail.com>
2020-08-18 13:43:30 +0100

    List steps to build Tic Tac Toe

--- bin/main.rb
+++ bin/main.rb
@@ -1,87 +1,27 @@
 #!/usr/bin/env ruby
-# Welcome users and show the board
 
-# current_board_positions = []
+# How to build a Working Tic Tac Toe Game User Interface Successfully
 
-puts 'Welcome to the tic-tac-toe game'
-
-puts 'Player One Insert Your name'
-
-# player1 = gets.chomp
-
-puts 'Player Two Enter your name'
-# player2 = gets.chomp
-
-# Below is where the board will be displayed
-
-puts '| || || |'
-puts '-----------'
-puts '| || || |'
-puts '-----------'
-puts '| || || |'
-puts 'The winner occupies any horizontal, vertical or diagonal positions concurrently'
-
-# Tell players the rules of the game and what is expected of them
-
-puts 'Letters, spaces and special characters are not allowed!! Choose a number from 1 to 9'
-
-# Display the current positions that are taken, at the begining it will be empty.
-
-# Method to Select which Player's turn
-class GameDynamics
-  def turn
-    @round = 0
-    while round < 9
-      if round.even?
-        # show free positions
-        puts "Player1 choose a free cell"
-        # check if answer is valid
-        # check if the cell is free
-        # return method that will display the board with the chosen position filled
-        # check if it matches a winning combination
-      else
-        # show free positions
-        puts "Player2 choose a free cell"
-        # check if answer is valid
-        # check if the cell is free
-        # return method that will display the board with the chosen position filled
-        # check if it matches a winning combination
-      end
-      round += 1
-    end
-  end
-
-  # Get inputs by player 1 and player 2
-
-  puts 'Here are the free positions:' # Following we will display the positions that can be taken
-
-  # player1_input = gets.chomp
-
-  # Check if the move is valid or not and inform the player if its not allowed.
-  def valid_move
-    unless valid ? 'Hey this is not allowed!!' : 'Valid move'
-    end
-  end
-  # Move of player one displayed on the board
-  puts "Positions available: #{current_board_positions}"
-  puts 'Player Two its your turn'
-
-  # player2_input = gets.chomp
-  # Display the board progress
-  puts "Positions available: #{current_board_positions}"
-  # Check which player is to play next
-
-  # Check Winner, if game is not a draw and spaces are all occupied
-  def winner
-    while !winner && turn_counter < 9
-      # repeat player selection loop
-      if winner == player1
-        puts "Congratulations #{player1} won"
-      elsif winner == player2
-        puts "Congratulations #{player2} won"
-      else
-        puts "It's a draw!!"
-      end
-    end
-  end
-end
+# 1. Create a class where we can initialize some variable that can be accessed throughout the game
+# 2. Create an initialized method where we can access the board, player and counter
+# 3. Create a winning combination. Here we would have a constant to represent 
+# the rows, columns and diagonals
+# 4. Use the winning combination constant to define a mothod to detect a winner
+# 5. Use the winner method and turn counter to define a draw method
+# 6. Define a method to display the board
+#  Define a DRAW method
+# 7. Define a player selection method that selects a player and runs the game logic by checking
+# for various conditions using a while loop with various if and else statements
+#  Various conditions to be evaluated in the WHILE LOOP while Selecting PLAYERS
+# A. Ab intio set the default turn counter to be equal to 1
+# B. For the WHILE loop while the turn counter less than or equal to 10 perform these actions
+#  I. If turn cunter is EVEN set the PLAYER to be equal to 'X'
+# a. Tell PLAYER X it's your turn and get input for PLAYER X 
+# b. Also convert PLEYER X input to the corresponding INDEX Board position and save in a variable
+# c. Create a nested CONDITIONAL to check if the BOARD position is equivalent to an empty STRING ' '
+# d. If the INDEX is an empty STRING replace the empty STRING with the PLAYER
+# e. Display the CURRENT BOARD with its contents and this shows the available POSITIONS to the PLAYERS
+# f.Check If there is a WINNING Postion to be assigned to a Winner, If there is a WINNER, display BOARD
+# break game after displaying the WINNER 
+# g. IF no WINNER check for a DRAW, if there is a draw BREAK from LOOP and tell the players its a draw game,
+#  if no DRAW at this point INCREAMENT the COUNTER
