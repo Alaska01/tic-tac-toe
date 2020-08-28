c4c00efc1aef2eff78e3bf87371a592bc095224c
Alaska01 <aadaniel108@gmail.com>
2020-08-28 18:55:25 +0100

    require file and state rules of game

--- bin/main.rb
+++ bin/main.rb
@@ -1,2 +1,11 @@
 #!/usr/bin/env ruby
 
+require_relative '..\lib\board'
+
+puts 'Welcome to Tic Tac Toe Game'
+puts 'Here are the game rules'
+puts 'Only enter numbers from the range of 1 to 9'
+puts 'Make sure board position is empty while considering the choice of your range number'
+puts 'The first to consecutively occuppy any of rows, columns or diagonals is decleared winner'
+puts 'If board positions are filled up with no winner, its a draw game'
+puts 'Bests of luck as you proceed and play the game'
